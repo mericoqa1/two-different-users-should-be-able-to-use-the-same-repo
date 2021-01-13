f146d10bbc0e8f9e1391edffe91d68a5c9a38b66
mericoqa2 <75958071+mericoqa2@users.noreply.github.com>
2021-01-13 12:03:50 -0800

    Update two-different-users-should-be-able-to-use-the-same-repo.py

--- two-different-users-should-be-able-to-use-the-same-repo.py
+++ two-different-users-should-be-able-to-use-the-same-repo.py
@@ -2,4 +2,8 @@
 def my_first_function():
     print("Hello from my_first_function")
 
+def my_second_function():
+    print("Hello from my_second_function")
+
 my_first_function()
+my_second_function()
